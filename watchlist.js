76e55876efdf13e1e1e2bc2a4db43ddf8f32adad
Krissy Conant <krissy@memberclicks.com>
2018-04-17 21:14:34 -0400

    updated to remove items on watch list

--- watchlist.js
+++ watchlist.js
@@ -2,6 +2,9 @@ var watchlistJSON = localStorage.getItem("watchlist");
 var watchlist = JSON.parse(watchlistJSON);
 
 $(function() {
+    var movieResults = renderMovies(watchlist);
+    $(".card-deck").append(movieResults);
+
     function renderMovies(watchlist) {
         var finalHTML = "";  
         watchlist.forEach(function(currentMovie) {
@@ -12,6 +15,7 @@ $(function() {
             finalHTML += "<h5 class='title card-title'>" + currentMovie.Title + "</h5>";
             finalHTML += "<h6 class='year'>Released: " + currentMovie.Year + "</h6>";
             finalHTML += "<div class='button card-footer'>";
+            finalHTML += "<button data-id='" + currentMovie.imdbID + "'class='removeMovie btn btn-primary'>Remove me!</button>";
             finalHTML += "</div>"
             finalHTML += "</div>"
             finalHTML += "</div>"
@@ -19,6 +23,24 @@ $(function() {
         });
         return finalHTML;
     }; 
-        var movieResults = renderMovies(watchlist);
-        $(".card-deck").append(movieResults);
+        
+    $(".card-deck").on("click", ".removeMovie", function() {
+        var imdbID = $(this).data("id");
+        watchlist = watchlist.filter(function(currentMovie) {
+            return currentMovie.imdbID != imdbID;
+        });
+        
+        watchlistJSON = JSON.stringify(watchlist);
+        localStorage.setItem("watchlist", watchlistJSON);
+
+        $(this).parent().parent().parent().parent().remove();
+    });
+
 });
+
+/* 
+true false or 1 0
+update the keys in watchlist.js - on click listener
+.find movie and give extra property currentMovie.rating 
+add HTML in renderMovies 
+*/
